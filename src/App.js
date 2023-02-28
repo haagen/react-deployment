6bc0ca178c8e85ce1a79d84079205675942f7715
Martin Haagen <martin@mbh.se>
2023-02-28 13:21:40 +0100

    Added a new message on the homepage.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{ margin: '0 auto', paddingTop: '100px' }}>
-      <strong >
-        This is my application that I have deployed!
-      </strong>
+      <p>
+        <strong >
+          This is my application that I have deployed!
+        </strong>
+      </p>
+      <p>
+        And it eaven updates automatically when changed in git.  
+      </p>  
     </div>
   );
 }
