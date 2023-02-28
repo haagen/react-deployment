d161b23ce5c8b602fb7516eedd8cd05ab7c91de9
Martin Haagen <martin@mbh.se>
2023-02-28 10:40:15 +0100

    First release of the beautiful application.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: '0 auto', paddingTop: '100px' }}>
+      <strong >
+        This is my application that I have deployed!
+      </strong>
     </div>
   );
 }
